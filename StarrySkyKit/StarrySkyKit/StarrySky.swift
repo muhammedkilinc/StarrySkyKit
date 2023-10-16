b11c4609f15e99805145f10fb8492034799846c6
Muhammed Kılınç <muhammedkilinc@Muhammeds-MBP.bbrouter>
2023-10-16 23:37:18 +0300

    Initialize project

---
+++ StarrySkyKit/StarrySkyKit/StarrySky.swift
@@ -0,0 +1,8 @@
+//
+//  StarrySky.swift
+//  StarrySkyKit
+//
+//  Created by Muhammed Kılınç on 16.10.2023.
+//
+
+import Foundation
---
+++ StarrySkyKit/StarrySkyKit/StarrySkyKit.h
@@ -0,0 +1,18 @@
+//
+//  StarrySkyKit.h
+//  StarrySkyKit
+//
+//  Created by Muhammed Kılınç on 16.10.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StarrySkyKit.
+FOUNDATION_EXPORT double StarrySkyKitVersionNumber;
+
+//! Project version string for StarrySkyKit.
+FOUNDATION_EXPORT const unsigned char StarrySkyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StarrySkyKit/PublicHeader.h>
+
+
