d4589f51263dbdcc79a012a25afdb59902ef749c
Muhammed Kılınç <muhammedkilinc@Muhammeds-MBP.bbrouter>
2023-10-17 01:04:38 +0300

    Add interface layer to show StarView from outside

--- StarrySkyKit/StarrySkyKit/StarrySky.swift
+++ StarrySkyKit/StarrySkyKit/StarrySky.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import UIKit
+
+public class StarrySky {
+
+    // Singleton instance
+    public static let shared = StarrySky()
+
+    // The main view of the framework
+    private lazy var starView: StarView = {
+        let view = StarView(skyManager: SkyManager())
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    // Private initializer to ensure only one instance is created
+    private init() {}
+
+    // Public method to add the StarView
+    public func addStarInterface(to parentView: UIView) {
+        parentView.addSubview(starView)
+        starView.anchorToSuperview()
+    }
+}
