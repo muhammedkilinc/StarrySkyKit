c034bc6edb748981c4d5fb926b96a8dfeafc82a4
Muhammed Kılınç <muhammedkilinc@Muhammeds-MBP.bbrouter>
2023-10-17 00:37:47 +0300

    Add SkyManager to implement sky managing

--- StarrySkyKit/StarrySkyKit/Model/SkyManager.swift
+++ StarrySkyKit/StarrySkyKit/Model/SkyManager.swift
@@ -12,3 +12,34 @@ protocol SkyManaging {
     func addStar(size: StarSize)
     func resetStars()
 }
+
+final class SkyManager {
+    private var stars: [Star] = []
+
+    private func printStars() {
+        print(stars)
+        let brightStars = stars.filter { $0.brightness == .bright }.count
+        print("Number of Bright stars: \(brightStars)")
+    }
+}
+
+extension SkyManager: SkyManaging {
+    func isSkyFull() -> Bool {
+        stars.count >= 10
+    }
+
+    func addStar(size: StarSize) {
+        guard let randomColor = StarColor.allCases.randomElement(),
+              let randomBrightness = StarBrightness.allCases.randomElement() else {
+            return
+        }
+        let star = Star(size: size, color: randomColor, brightness: randomBrightness)
+        stars.append(star)
+        printStars()
+    }
+
+    func resetStars() {
+        stars.removeAll()
+        printStars()
+    }
+}
