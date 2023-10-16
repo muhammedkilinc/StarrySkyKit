62ebafc2b072071b468df82c57b08876eb933744
Muhammed Kılınç <muhammedkilinc@Muhammeds-MBP.bbrouter>
2023-10-17 02:08:51 +0300

    Support describable for meaningful logs

--- StarrySkyKit/StarrySkyKit/Model/Star.swift
+++ StarrySkyKit/StarrySkyKit/Model/Star.swift
@@ -7,20 +7,55 @@
 
 import Foundation
 
-enum StarColor: Codable, CaseIterable {
+enum StarColor: Codable, CaseIterable, CustomStringConvertible {
     case red, blue, green
+
+    var description: String {
+        switch self {
+        case .red:
+            "Red"
+        case .blue:
+            "Blue"
+        case .green:
+            "Green"
+        }
+    }
 }
 
-enum StarBrightness: Codable, CaseIterable {
+enum StarBrightness: Codable, CaseIterable, CustomStringConvertible {
     case bright, normal
+
+    var description: String {
+        switch self {
+        case .bright:
+            "Bright"
+        case .normal:
+            "Not so much"
+        }
+    }
 }
 
-enum StarSize: Codable {
+enum StarSize: Codable, CustomStringConvertible {
     case small, big
+
+    var description: String {
+        switch self {
+        case .small:
+            "Small"
+        case .big:
+            "big"
+        }
+    }
 }
 
 public struct Star: Codable {
     let size: StarSize
     let color: StarColor
     let brightness: StarBrightness
+}
+
+extension Star: CustomStringConvertible {
+    public var description: String {
+        "size: \(size) - color: \(color) - bright: \(brightness)"
+    }
 }
